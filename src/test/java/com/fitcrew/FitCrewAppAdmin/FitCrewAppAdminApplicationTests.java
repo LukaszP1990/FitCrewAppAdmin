9edf25470d4831fae37d9082c6933ef95857b6b7
Łukasz <lukaszptaszek@interia.com>
2019-09-13 07:34:44 +0200

    admin ws

---
+++ src/test/java/com/fitcrew/FitCrewAppAdmin/FitCrewAppAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fitcrew.FitCrewAppAdmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FitCrewAppAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
